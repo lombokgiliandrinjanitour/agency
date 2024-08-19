1a03ca6e42dfd116d7d230c0627bfaad25bbee30
lombokgiliandrinjanitour <herususanto.st@gmail.com>
2024-08-20 06:13:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function order(packageName) {
     const phoneNumber = '62817366355';
-    const message = `Halo, I like to order  ${packageName}.`;
+    const message = `https://lombokgiliandrinjanitour.github.io/agency/ Halo, I like to order  ${packageName}.`;
     const waUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(waUrl, '_blank');
 }
